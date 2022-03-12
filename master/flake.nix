2c07f4052d3327aea7befa9374f37135b6cd7af7
hugosenari <hugosenari@gmail.com>
2022-03-12 04:09:45 -0300

    chore: re index simplepng

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''high level simple way to write PNGs'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-simplepng-master.flake = false;
+  inputs.src-simplepng-master.owner = "jrenner";
+  inputs.src-simplepng-master.ref   = "refs/heads/master";
+  inputs.src-simplepng-master.repo  = "nim-simplepng";
+  inputs.src-simplepng-master.type  = "github";
+  
+  inputs."nimpng".dir   = "nimpkgs/n/nimpng";
+  inputs."nimpng".owner = "riinr";
+  inputs."nimpng".ref   = "flake-pinning";
+  inputs."nimpng".repo  = "flake-nimble";
+  inputs."nimpng".type  = "github";
+  inputs."nimpng".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimpng".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-simplepng-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-simplepng-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
